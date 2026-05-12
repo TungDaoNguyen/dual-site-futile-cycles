5ace9df5fc4ecb65356ab89ac4330340ab759c7f
badaljoshi <badjos@gmail.com>
2026-05-12 16:42:15 -0700

    Update Generate_ec1plot.m

--- MatlabPlotting/Generate_ec1plot.m
+++ MatlabPlotting/Generate_ec1plot.m
@@ -189,7 +189,7 @@ set(gcf,'Units','Inches');
 pos = get(gcf,'Position');
 set(gcf,'PaperPositionMode','Auto','PaperUnits','Inches','PaperSize',[pos(3),pos(4)]);
 
-savePath = '/Users/bjoshi/Library/CloudStorage/Dropbox/BifuncMSS/MatlabPlotting/';
+savePath = 'MatlabPlotting';
 print([savePath 'ec1_substrates.png'],'-dpng','-r300');
 print([savePath 'ec1_substrates.pdf'],'-dpdf','-r300','-painters');
 
